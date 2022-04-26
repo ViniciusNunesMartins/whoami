19230a9edea7ae13bbad911709ea4296e2f02b04
Vinicius Nunes Martins <nunesmartinsvinicius@gmail.com>
2022-04-26 20:33:56 -0300

    Add links in footer

--- src/components/footer/index.tsx
+++ src/components/footer/index.tsx
@@ -1,11 +1,33 @@
 import { FC } from 'react';
-import { FooterContainer } from './styles';
+import { FooterContainer, Links, Block } from './styles';
+import { FaCode, FaGithub, FaLinkedin } from 'react-icons/fa';
 
 
 const Footer: FC = () => {
   return (
     <FooterContainer>
-      <a href="https://github.com/viniciusnunesmartins/whoami">src on here</a>
+      <Block>
+        <Links>
+          <li>
+            <a href="https://github.com/viniciusnunesmartins">
+              <FaGithub size={24} />
+            </a>
+            <span>My GitHub</span>
+          </li>
+          <li>
+            <a href="https://www.linkedin.com/in/viniciusnunesmartins/">
+              <FaLinkedin size={24} />
+            </a>
+            <span>My LinkedIn</span>
+          </li>
+          <li>
+            <a href="https://github.com/viniciusnunesmartins/whoami">
+              <FaCode size={24} />
+            </a>
+            <span>Whoami Repository</span>
+          </li>
+        </Links>
+      </Block>
     </FooterContainer>
   );
 };
--- src/components/footer/styles.ts
+++ src/components/footer/styles.ts
@@ -1,11 +1,10 @@
-import styled from 'styled-components';
+import styled, { keyframes } from 'styled-components';
 
 
 export const FooterContainer = styled.footer`
   background-color: ${props => props.theme.colors.primary};
   color: ${props => props.theme.colors.text};
   padding: 2rem;
-  text-align: center;
   font-size: 0.8rem;
   font-weight: bold;
   position: absolute;
@@ -13,4 +12,45 @@ export const FooterContainer = styled.footer`
   left: 0;
   right: 0;
   margin-top: 2rem;
+`;
+
+export const Block = styled.div`
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  margin-bottom: 1rem;
+`;
+
+const showText = keyframes`
+  0% {
+    opacity: 0;
+  }
+  100% {
+    opacity: 1;
+  }
+`;
+
+export const Links = styled.ul`
+  color: ${props => props.theme.colors.background};
+  list-style: none;
+  li {
+    display: flex;
+    align-items: center;
+    span {
+      display: none;
+    }
+    &:hover {
+      span {
+        animation: ${showText} 0.5s ease-in-out;
+        margin-left: 5px;
+        display: inline;
+      }
+    }
+  }
+
+  a {
+    text-decoration: none;
+    color: ${props => props.theme.colors.background};
+    &:hover {}
+  }
 `;
